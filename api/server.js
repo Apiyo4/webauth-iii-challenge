da6ca305ee2c2b84da9fd4ff0c8659e38c99f925
Annette Odingo <odingoannette@gmail.com>
2020-01-29 12:37:19 +0000

    Create user router, configure middleware and ensure userrouter works

--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
+const configureMiddleware = require('./configure-midleware');
+const UserRouter = require('../users/user-router');
+
 const server = express();
+
+configureMiddleware(server); 
+
+server.use('/api', UserRouter);
 
 server.get('/', (req,res)=>{
     res.json(`Api is working`)
