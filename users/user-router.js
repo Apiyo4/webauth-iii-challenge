59ef9e848df640b3ef34a91ca566be482e409d57
Annette Odingo <odingoannette@gmail.com>
2020-01-29 12:51:55 +0000

    Create a route with path /login that checks if user is in the database and returns the user

--- users/user-router.js
+++ users/user-router.js
@@ -1,23 +1,37 @@
-const express = require('express');
-const Users=  require('./users-model');
+const express = require("express");
+const Users = require("./users-model");
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-    const { username, password, department } = req.body;
-    const user = {
-      username,
-      password, 
-      department
-    };
-  
-    Users.add(user)
-      .then(saved => {
-        res.status(201).json(saved);
+router.post("/register", (req, res) => {
+  const { username, password, department } = req.body;
+  const user = {
+    username,
+    password,
+    department
+  };
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+    Users.findBy({ username, password })
+      .first()
+      .then(user => {
+        if (user && password) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
       })
       .catch(error => {
         res.status(500).json(error);
       });
   });
 
-module.exports= router;
+module.exports = router;
