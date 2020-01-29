46cb62bc4bf9ab746fb82e9b89b64db68e1f76ca
Annette Odingo <odingoannette@gmail.com>
2020-01-29 12:46:08 +0000

    Add a route with path /register that registers a new use

--- users/user-router.js
+++ users/user-router.js
@@ -3,6 +3,21 @@ const Users=  require('./users-model');
 
 const router = express.Router();
 
-
+router.post('/register', (req, res) => {
+    const { username, password, department } = req.body;
+    const user = {
+      username,
+      password, 
+      department
+    };
+  
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
 module.exports= router;
